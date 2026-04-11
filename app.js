f0c6c2274cae04791ba16c50d1fb73185179b216
Chenevoix Clemence <cchenevoix@gmail.com>
2026-04-11 21:38:24 +0200

    REFACTOR(display prompts): modify the function render to avoid repeating process

--- app.js
+++ app.js
@@ -6,36 +6,19 @@ fetch("./prompts.json").then((reponse) => {
         let category = ""
         let prompt = ""
         while (index < json.length){
-            title = document.createTextNode(json[index].title);
-            category = document.createTextNode(json[index].category);
-            prompt = document.createTextNode(json[index].prompt);
-
-            let td_1 = document.createElement("td");
-            td_1.appendChild(title);
-            let td_2 = document.createElement("td");
-            td_2.appendChild(category);
-            let td_3 = document.createElement("td");
-            td_3.appendChild(prompt);
-            let to_append = document.createElement("tr");
-            to_append.appendChild(td_1);
-            to_append.appendChild(td_2);
-            to_append.appendChild(td_3);
-            document.getElementById('Table_json_prompts').appendChild(to_append);
+            render(json[index].title, json[index].category, json[index].prompt, 'Table_json_prompts')
             index += 1
         } 
     });
 });
 
-function render(title_value, category_value, prompt_value) {
-    console.log("test")
+function render(title_value, category_value, prompt_value, table_id) {
     let title = ""
     let category = ""
     let prompt = ""
-
     title = document.createTextNode(title_value);
     category = document.createTextNode(category_value);
     prompt = document.createTextNode(prompt_value);
-
     let td_1 = document.createElement("td");
     td_1.appendChild(title);
     let td_2 = document.createElement("td");
@@ -46,13 +29,11 @@ function render(title_value, category_value, prompt_value) {
     to_append.appendChild(td_1);
     to_append.appendChild(td_2);
     to_append.appendChild(td_3);
-    document.getElementById('Table_prompts_to_add').appendChild(to_append);
+    document.getElementById(table_id).appendChild(to_append);
 }
-
 form = document.getElementById("Prompt_form");
-
 form.onsubmit = function(question) {
     question.preventDefault();
-    render(document.getElementById("title").value, document.getElementById("category").value, document.getElementById("prompt").value);
+    render(document.getElementById("title").value, document.getElementById("category").value, document.getElementById("prompt").value, 'Table_prompts_to_add');
     form.reset();
 }
